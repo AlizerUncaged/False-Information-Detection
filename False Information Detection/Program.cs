a388238ddfb36ce2722eb5316444d5a066d3d907
floyd <saylent1@gmail.com>
2022-10-01 20:15:14 +0800

    Add project files.

---
+++ False Information Detection/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AI
+{
+    /// <summary>
+    /// An AI for detecting false news using machine learning
+    /// algorithms.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main entry point of the program.
+        /// </summary>
+        public async Task StartAsync()
+        {
+
+        }
+
+        static void Main(string[] args) => new Program()
+            .StartAsync()
+            .GetAwaiter()
+            .GetResult();
+    }
+}
