3d01e5776b8b4d04fc93ac9b22a36e6dc1d0b919
floyd <saylent1@gmail.com>
2022-10-05 22:11:54 +0800

    ✨ Added console input.

--- False Information Detection/Program.cs
+++ False Information Detection/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace AI
+﻿using False_Information_Detection;
+
+namespace AI
 {
     /// <summary>
     /// An AI for detecting false news using machine learning
@@ -11,7 +13,20 @@
         /// </summary>
         public async Task StartAsync()
         {
+            Console.Write("News Article Title: ");
+            var title = Console.ReadLine();
+            Console.Write("News Article Text: ");
+            var text = Console.ReadLine();
+            // Load sample data
+            var sampleData = new FakeNewsConfiguration.ModelInput()
+            {
+                Title = title,
+                Text = text,
+            };
 
+            // Load model and predict output
+            var result = FakeNewsConfiguration.Predict(sampleData);
+            Console.WriteLine($"Prediction: {(result.Prediction == 0 ? "Reliable" : "Most Likely Fake")}");
         }
 
         static void Main(string[] args) => new Program()
